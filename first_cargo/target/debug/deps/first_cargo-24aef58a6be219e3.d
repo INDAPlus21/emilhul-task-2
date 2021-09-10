a45e97cc95b9c161af7b5b58780cc8daa38848fa
Emil Hultcrantz <emilhul@kth.se>
2021-09-10 19:40:52 +0200

    Experimenting with rust * Moves rust files into seperate folder * Begin implementing guessing_game from "the book"

---
+++ first_cargo/target/debug/deps/first_cargo-24aef58a6be219e3.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/emilh/homemade/repos/emilhul-task-2/first_cargo/target/debug/deps/first_cargo-24aef58a6be219e3: src/main.rs
+
+/mnt/c/Users/emilh/homemade/repos/emilhul-task-2/first_cargo/target/debug/deps/first_cargo-24aef58a6be219e3.d: src/main.rs
+
+src/main.rs:
---
+++ first_cargo/target/debug/first_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/emilh/homemade/repos/emilhul-task-2/first_cargo/target/debug/first_cargo: /mnt/c/Users/emilh/homemade/repos/emilhul-task-2/first_cargo/src/main.rs
