cd49e0c15badd8235892d6a21bfd27ff87f65707
Emil Hultcrantz <emilhul@kth.se>
2021-09-13 18:12:50 +0200

    Finnished guessing game

--- first_cargo/src/main.rs
+++ first_cargo/src/main.rs
@@ -1,31 +1,38 @@
-use std::io;                                            // Library for handeling standard input/output
-use rand::Rng;                                          // Library for simulating randomness
-use std::cmp::Ordering;                                 // Library for handling comparisons
+use std::io;                                                // Library for handeling standard input/output
+use rand::Rng;                                              // Library for simulating randomness
+use std::cmp::Ordering;                                     // Library for handling comparisons
 
 fn main() {
-    println!("Guess the number!");                      // Prints to the console line
+    println!("Guess the number!");                          // Prints to the console line
 
     let secret_number = rand::thread_rng()
-        .gen_range(1..101);                             // Creates a variable called secret_number and asigns it a random value between 1-100
-
-    println!("The secret number is {}", secret_number);
-    println!("Please iput your guess.");
-
-    let mut guess = String::new();                      // Creates a mutable variable called guess and assigns it to a new empty string
-
-    io::stdin()                                         // Calls stdin function that handles standard input for the terminal.
-        .read_line(&mut guess)                          // Calls the read_line function that takes the input and appends it to a string. &mut guess is a mutable reference to the guess variable
-        .expect("Failed to read line");                 // If the read_line function would fail expect would crash the program and display the error message
-
-    let guess: u32 = guess                              // Converts guess into an unsigned 32 bit number 
-        .trim().parse()
-        .expect("Please type a numbrer!");
+        .gen_range(1..101);                                 // Creates a variable called secret_number and asigns it a random value between 1-100
     
-    println!("You guessed: {}", guess);                 // {} is a countainer for a variable. In this case guess
+    loop {
+        println!("Please iput your guess.");
 
-    match guess.cmp(&secret_number) {                   // A match function looks trough its so called arns and decides on the one matching the results of cmp (compare)
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let mut guess = String::new();                      // Creates a mutable variable called guess and assigns it to a new empty string
+
+        io::stdin()                                         // Calls stdin function that handles standard input for the terminal.
+            .read_line(&mut guess)                          // Calls the read_line function that takes the input and appends it to a string. &mut guess is a mutable reference to the guess variable
+            .expect("Failed to read line");                 // If the read_line function would fail expect would crash the program and display the error message
+
+        let guess: u32 = match guess                        // Converts guess into an unsigned 32 bit number, if possivle otherwise the game goes on
+            .trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+    
+        println!("You guessed: {}", guess);                 // {} is a countainer for a variable. In this case guess
+
+        match guess.cmp(&secret_number) {                   // A match function looks trough its so called arns and decides on the one matching the results of cmp (compare)
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+    
 }
