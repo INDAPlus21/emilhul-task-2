e804d18d3f5b6f0450158c77e087343f567ea367
Emil Hultcrantz <emilhul@kth.se>
2021-09-15 10:46:46 +0200

    Adds proof and tidies up the code

--- summera_tal/src/main.rs
+++ summera_tal/src/main.rs
@@ -1,30 +1,31 @@
 use std::io;
-use std::io::prelude::*;
+use std::io::BufRead;
 
-fn main() {                                                 // Kattis calls main function to run your solution.
-    let input = io::stdin();                                // get standard input stream                                                         
+fn main() {
+    let input = io::stdin();                                    // get standard input stream                                                         
     
-    let mut numbers: Vec<usize> = input
+    let mut numbers = input
         .lock()
         .lines()   
-        .skip(1) // read only number line
+        .skip(1)                                                // skip line telling how many numbers and read only number line
         .next().unwrap().ok().unwrap()
         .split_whitespace()
         .map(|_number| _number.parse::<usize>().ok().unwrap())
-        .collect();
+        .collect::<Vec<usize>>();                               // collect the numbers into a vector.
     
-    let _n = numbers.len();
+    let _n = numbers.len();                                     
 
     if _n == 0 {
-        println!("0");
+        println!("0");                                          // if we get an empty line len will be 0, we then return the sum of nothing (0)
     } else if _n > 0 {
-        let to_sum = (_n+1)/2;
+        let n_sum = (_n+1)/2;                                   // due to how integer division this will be equal to _n/2 for even numbers and (_n+1)/2 for odd ones
+        
         numbers.sort();
+        let num_to_sum = &numbers[(_n-n_sum)..];                // get only the n_sum largest numbers from the vector
 
-        let big_half = &numbers[(_n-to_sum)..];
-        let sum: usize = big_half.iter().sum();
+        let sum: usize = num_to_sum.iter().sum();               // iterate over the vecotr containing the largest numbers, adding all the elements together
         println!("{}", sum);
-    } else {
+    } else {                                                    // should never execute since the length of numbers can only ever be 0 or >0
         println!("0");
     }
 }
