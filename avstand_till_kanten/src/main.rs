1ceab92e61533ac73dee24649f3d5558ae98504b
Emil Hultcrantz <emilhul@kth.se>
2021-09-18 00:35:38 +0200

    Optimization of the avstand_till_kanten solution

--- avstand_till_kanten/src/main.rs
+++ avstand_till_kanten/src/main.rs
@@ -17,21 +17,22 @@ fn main() {
     let cols: usize = values[1];
 
     for x in 1..=rows {                                                                 // for every row and every col
+        let mut row: String = String::with_capacity(cols);                              // create a string for the row
         for y in 1..=cols {
             let min_dist = cmp::min(cmp::min(x, rows-x+1), cmp::min(y, cols-y+1));      // get the smallest distance
-            match min_dist {
-                1 => print!("1"),
-                2 => print!("2"),
-                3 => print!("3"),
-                4 => print!("4"),
-                5 => print!("5"),
-                6 => print!("6"),
-                7 => print!("7"),
-                8 => print!("8"),
-                9 => print!("9"),
-                _ => print!(".")
+            match min_dist {                                                            // match the distane to the char and push it to the row string
+                1 => row.push('1'),
+                2 => row.push('2'),
+                3 => row.push('3'),
+                4 => row.push('4'),
+                5 => row.push('5'),
+                6 => row.push('6'),
+                7 => row.push('7'),
+                8 => row.push('8'),
+                9 => row.push('9'),
+                _ => row.push('.')
             }
         }
-        println!();                                                                     // print empty line, skipping to the next one
+        println!("{}", row);                                                           // print the row string, and continue on a new line
     } 
 }
